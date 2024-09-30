7e6b10fc5f61ed76dd5f2f60ce92069891c754d5
Kevin Lin <minecrafts96628@gmail.com>
2024-09-30 17:07:37 +0800

    create landing page first section gsap

--- src/components/GSAP/GSAPWrapper.tsx
+++ src/components/GSAP/GSAPWrapper.tsx
@@ -1,34 +1,167 @@
 import React, { useRef, useEffect, useState } from "react";
 import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 import styled from "styled-components";
-import centerImg from "./circle.png";
-import cubeImg from "./Cube.png";
-import standingMan from "./standingMan.png";
+import asparagus from "./asparagus.png";
 import avocado from "./avocado.png";
+import banana from "./banana.png";
+import cheery from "./cheery.png";
+import lemon from "./lemon.png";
+import garlic from "./garlic.png";
+import potato from "./potato.png";
+import tomato from "./tomato.png";
+import pear from "./pear.png";
+import pineapple from "./pineapple.png";
+import redCarrot from "./redCarrot.png";
+
+// 使用相對單位來讓圖片具備 RWD 功能
 
 const GSAPWrapper: React.FC = () => {
   const wordRefs = useRef([]);
+  const imageRefs = useRef([]);
   const isCentered = useRef(false);
-  const centerImgRef = useRef(null);
-  const planetContainerRef = useRef(null);
-  const outerCircleRef = useRef(null);
-  const innerCircleRef = useRef(null);
-  const InnerCubeImgRef = useRef(null);
-  const OuterCubeImgRef = useRef(null);
-  const ellipseRef = useRef(null);
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
   const centerX = windowWidth / 2;
   const centerY = windowHeight / 2;
-  const isAnimating = useRef(false); // 用於鎖定動畫進行狀態
+  const isAnimating = useRef(false);
   const [scrollLocked, setScrollLocked] = useState(false);
-  // 定義四個單字
-  const nutritionWords = ["N", "U", "T", "R", "I", "T", "I", "O", "N"];
-  const balanceWords = ["B", "A", "L", "A", "N", "C", "E"];
 
-  // 定義Balance的字母
+  const nutritionWords = [
+    { letter: "N", zIndex: 0 },
+    { letter: "U", zIndex: 0 },
+    { letter: "T", zIndex: 5 },
+    { letter: "R", zIndex: 5 },
+    { letter: "I", zIndex: 0 },
+    { letter: "T", zIndex: 0 },
+    { letter: "I", zIndex: 10 },
+    { letter: "O", zIndex: 10 },
+    { letter: "N", zIndex: 10 },
+  ];
+
+  const balanceWords = [
+    { letter: "B", zIndex: 0 },
+    { letter: "A", zIndex: 0 },
+    { letter: "L", zIndex: 0 },
+    { letter: "A", zIndex: 0 },
+    { letter: "N", zIndex: 0 },
+    { letter: "C", zIndex: 0 },
+    { letter: "E", zIndex: 2 },
+  ];
+
+  const images = {
+    avocado: {
+      src: avocado,
+      width: "400vw",
+      height: "auto",
+      initial: { x: 100, y: -600 },
+      final: { x: centerX - 200, y: centerY + 250 },
+    },
+    potato: {
+      src: potato,
+      width: "250vw",
+      height: "auto",
+      initial: { x: 300, y: -800 },
+      final: { x: centerX - 80, y: centerY + 110 },
+      rotate: 0,
+      scale: 1,
+    },
+
+    tomato: {
+      src: tomato,
+      width: "150vw",
+      height: "auto",
+      initial: { x: 400, y: -900 },
+      final: { x: centerX - 40, y: centerY + 22 },
+      rotate: 0,
+      scale: 1,
+      zIndex: 1,
+    },
+    asparagus: {
+      src: asparagus,
+      width: "800vw",
+      height: "auto",
+      initial: { x: -200, y: -500 },
+      final: { x: centerX - 370, y: centerY - 50 },
+      rotate: -30,
+      scale: 1,
+      zIndex: 2,
+    },
+    pear: {
+      src: pear,
+      width: "130vw",
+      height: "auto",
+      initial: { x: centerX - 80, y: -500 },
+      final: { x: centerX - 80, y: centerY - 110 },
+      rotate: 0,
+      scale: 1,
+      zIndex: -1,
+    },
+
+    redCarrot: {
+      src: redCarrot,
+      width: "155vw",
+      height: "auto",
+      initial: { x: centerX - 330, y: windowHeight + 500 },
+      final: { x: centerX - 330, y: centerY + 235 },
+      scale: 1,
+      rotate: -60,
+      zIndex: 2,
+    },
+    banana: {
+      src: banana,
+      width: "210vw",
+      height: "auto",
+      initial: { x: centerX + 280, y: -700 },
+      final: { x: centerX + 280, y: 180 },
+      rotate: -10,
+      scale: 1,
+      zIndex: 2,
+    },
+    cheery: {
+      src: cheery,
+      width: "100px",
+      height: "auto",
+      initial: { x: centerX + 500, y: centerY + 100 },
+      final: { x: centerX + 510, y: centerY + 110 },
+      rotate: 15,
+      scale: 0,
+      scaleY: 0,
+    },
+
+    lemon: {
+      src: lemon,
+      width: "100vw",
+      height: "auto",
+      initial: { x: centerX + 335, y: -500 },
+      final: { x: centerX + 510, y: centerY + 10 },
+      rotate: 85,
+      scale: 1,
+      zIndex: -2,
+    },
+    pineapple: {
+      src: pineapple,
+      width: "80vw",
+      height: "auto",
+      initial: { x: centerX - 330, y: -500 },
+      final: { x: centerX - 280, y: centerY + 10 },
+      scale: 1,
+      rotate: 0,
+      zIndex: -1,
+    },
+    garlic: {
+      src: garlic,
+      width: "100px",
+      height: "auto",
+      initial: { x: 250, y: -200 },
+      final: { x: 250, y: centerY - 180 },
+      rotate: 0,
+      scale: 1,
+      zIndex: 10,
+    },
+  };
   const initialFontSizes = [
-    "2vw", // 左下角Ｎ
+    "2vw",
     "2.5vw",
     "2.3vw",
     "2.7vw",
@@ -47,116 +180,67 @@ const GSAPWrapper: React.FC = () => {
     "3vw",
   ];
 
-  const finalFontSizes = [
-    "7vw", // 左下角Ｎ
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-    "7vw",
-  ];
+  const finalFontSizes = Array(16).fill("7vw");
 
   const initialRotations = [30, -45, 60, -90, 45, -30, 15, -60, 90];
-  const finalRotations = Array(15).fill(0);
+  const finalRotations = Array(16).fill(0);
 
   const positions = [
-    { x: -500, y: centerY - 300 }, // 左下角Ｎ
-    { x: 100, y: 100 }, // 左上角U
-    { x: -100, y: -100 }, // 左下角T
-    { x: 300, y: -300 }, // 左上角R
-    { x: 200, y: windowHeight + 100 }, // 正中間I
-    { x: 100, y: -200 }, // 右T
-    { x: centerX, y: centerY }, // 右下角I
-    { x: centerX - 200, y: -300 }, // 右下角O
-    { x: centerX, y: -100 }, // 右上角N
-    { x: -100, y: 300 }, // 左上角B
-    { x: centerY, y: windowHeight + 100 }, // 上Ａ
-    { x: centerX - 300, y: centerY - 200 }, // 左Ｌ
-    { x: windowWidth, y: windowHeight - 300 }, // 左下Ａ
-    { x: centerX, y: windowHeight - 100 }, // 右上角小N
-    { x: centerX + 100, y: -100 }, // 左中C
-    { x: centerX, y: centerY }, // 右下角E
+    { x: -500, y: centerY - 300 },
+    { x: 100, y: 100 },
+    { x: -100, y: -100 },
+    { x: 300, y: -300 },
+    { x: 200, y: windowHeight + 100 },
+    { x: 100, y: -200 },
+    { x: centerX, y: centerY },
+    { x: centerX - 200, y: -300 },
+    { x: centerX, y: -100 },
+    { x: -100, y: 300 },
+    { x: centerY, y: windowHeight + 100 },
+    { x: centerX - 300, y: centerY - 200 },
+    { x: windowWidth, y: windowHeight - 300 },
+    { x: centerX, y: windowHeight - 100 },
+    { x: centerX + 100, y: -100 },
+    { x: centerX, y: centerY },
   ];
+
   const fontGap = windowWidth * 0.05; // 字母之間的間距
+
   const finalPositions = [
     ...nutritionWords.map((_, index) => ({
       x: index * fontGap - 75,
       y: centerY / 2 - 150,
     })),
     ...balanceWords.map((_, index) => ({
-      x: index * fontGap + 200, // 平衡行的 X 坐標往右偏移
-      y: centerY / 2 - 100,
+      x: index * fontGap + 350,
+      y: centerY / 2 - 70,
     })),
   ];
-  const initialOuterCircleRadius = centerX * 0.6;
-  const finalOuterCircleRadius = 0;
-  const initialInnerCircleRadius = centerX * 0.35;
-  const finalInnerCircleRadius = 0;
 
   useEffect(() => {
+    // 設定文字初始位置
     wordRefs.current.forEach((word, index) => {
       gsap.set(word, {
         x: positions[index].x,
         y: positions[index].y,
         rotate: initialRotations[index],
         fontSize: initialFontSizes[index],
+
+        zIndex:
+          index < nutritionWords.length
+            ? nutritionWords[index].zIndex
+            : balanceWords[index - nutritionWords.length].zIndex,
       });
     });
-    gsap.set(centerImgRef.current, {
-      x: centerX,
-      y: centerY,
-    });
-    gsap.set(planetContainerRef.current, {
-      x: -150,
-      y: windowHeight - 200,
-    });
 
-    gsap.set(outerCircleRef.current, {
-      rx: centerX,
-      ry: centerY,
-      r: initialOuterCircleRadius,
-    });
-
-    gsap.set(innerCircleRef.current, {
-      rx: centerX,
-      ry: centerY,
-      r: initialInnerCircleRadius,
-    });
-    gsap.set(OuterCubeImgRef.current, {
-      x: windowWidth - 500,
-      y: 100,
-      rotation: 0,
-    });
-    gsap.to(OuterCubeImgRef.current, {
-      rotation: -360,
-      duration: 10,
-      repeat: -1,
-      ease: "none",
-    });
-    gsap.set(InnerCubeImgRef.current, {
-      x: centerX - 500,
-      y: centerY + 100,
-      rotation: 0,
-    });
-    gsap.set(ellipseRef.current, {
-      x: 0,
-      y: -200,
-    });
-    gsap.to(InnerCubeImgRef.current, {
-      rotation: 360,
-      duration: 13,
-      repeat: -1,
-      ease: "none",
+    // 設定圖片初始位置
+    Object.keys(images).forEach((key, index) => {
+      gsap.set(imageRefs.current[index], {
+        x: images[key].initial.x,
+        y: images[key].initial.y,
+        scale: images[key].scale,
+        rotate: images[key].scale,
+      });
     });
 
     const handleScroll = (event) => {
@@ -164,10 +248,10 @@ const GSAPWrapper: React.FC = () => {
       if (isAnimating.current) return;
 
       if (scrollDirection === "down" && !isCentered.current) {
-        // 限制滾動
         setScrollLocked(true);
         isAnimating.current = true;
 
+        // 文字動畫
         wordRefs.current.forEach((word, index) => {
           gsap.to(word, {
             duration: 1,
@@ -180,69 +264,46 @@ const GSAPWrapper: React.FC = () => {
         });
 
         // 圖片動畫
-
-        gsap.to(planetContainerRef.current, {
-          x: centerX - 200,
-          y: centerY - 300,
-          scale: 0,
-          duration: 1,
-          ease: "power2.out",
+        Object.keys(images).forEach((key, index) => {
+          gsap.to(imageRefs.current[index], {
+            x: images[key].final.x,
+            y: images[key].final.y,
+            duration: 1.5,
+            scale: images[key].scale, // cheery 從 0 放大到 1
+            rotate: images[key].rotate,
+            delay: index * 0.2, // 增加延遲以達到漸進效果
+            zIndex: images[key].zIndex,
+            ease: "power4.out",
+            onComplete: () => {
+              if (index === Object.keys(images).length - 1) {
+                isAnimating.current = false;
+                setScrollLocked(false);
+              }
+            },
+          });
         });
-
-        // 圓動畫
-        gsap.to(outerCircleRef.current, {
-          rx: 0,
-          ry: 0,
-          r: finalOuterCircleRadius,
-          duration: 0,
-          ease: "power4.out",
-        });
-
-        gsap.to(innerCircleRef.current, {
-          rx: 0,
-          ry: 0,
-          r: finalInnerCircleRadius,
-          duration: 0,
-          ease: "power4.out",
-        });
-        gsap.to(ellipseRef.current, {
-          rx: centerX * 0.8,
-          ry: centerY * 0.4,
-          duration: 0,
-          ease: "power4.out",
-        });
-        gsap.to(OuterCubeImgRef.current, {
-          x: centerX - 100, // 聚集到中心
-          y: centerY - 100,
-          scale: 0, // 可以進行縮放
-          duration: 1,
-          ease: "power4.out",
-        });
-
-        gsap.to(InnerCubeImgRef.current, {
-          x: centerX - 100, // 聚集到中心
-          y: centerY - 100,
-          scale: 0,
-          duration: 1,
-
-          ease: "power4.out",
-        });
-        gsap.to(centerImgRef.current, {
-          scale: 2,
-          y: centerY - 100,
-          duration: 1,
+        gsap.to(imageRefs.current[10], {
+          duration: 2,
+          delay: 2,
+          x: images.garlic.final.x,
+          y: images.garlic.final.y,
+          ease: "bounce.out",
           onComplete: () => {
             isAnimating.current = false;
             setScrollLocked(false);
           },
-          ease: "power1.out",
         });
 
         isCentered.current = true;
+        setScrollLocked(false);
       }
 
-      if (scrollDirection === "up" && isCentered.current) {
-        // 返回初始文字動畫
+      if (
+        scrollDirection === "up" &&
+        isCentered.current &&
+        window.scrollY === 0
+      ) {
+        // 返回初始文字位置
         wordRefs.current.forEach((word, index) => {
           gsap.to(word, {
             duration: 1,
@@ -254,72 +315,23 @@ const GSAPWrapper: React.FC = () => {
           });
         });
 
-        // 圖片動畫
-        gsap.to(centerImgRef.current, {
-          scale: 1,
-          y: centerY,
-          rotate: 0,
-          duration: 1,
-          delay: 0.5,
-          onComplete: () => {
-            isAnimating.current = false;
-          },
-          ease: "power4.out",
-        });
-        gsap.to(planetContainerRef.current, {
-          x: -200,
-          y: windowHeight - 200,
-          scale: 1,
-          duration: 2,
-          ease: "power2.out",
-        });
-
-        gsap.to(outerCircleRef.current, {
-          cx: centerX,
-          cy: centerY,
-          y: 0,
-          r: initialOuterCircleRadius,
-          duration: 0,
-          ease: "power4.out",
-        });
-
-        gsap.to(innerCircleRef.current, {
-          cx: centerX,
-          cy: centerY,
-          y: 0,
-          r: initialInnerCircleRadius,
-          duration: 0,
-          ease: "power4.out",
-        });
-        gsap.to(ellipseRef.current, {
-          rx: 0,
-          ry: 0,
-          y: -200,
-          ease: "power4.out",
-        });
-        gsap.to(OuterCubeImgRef.current, {
-          x: windowWidth - 500,
-          y: 100,
-          scale: 1,
-          duration: 2,
-          ease: "power2.out",
-        });
-
-        gsap.to(InnerCubeImgRef.current, {
-          x: centerX - 500,
-          y: centerY + 100,
-          scale: 1,
-          duration: 2,
-          ease: "power2.out",
+        // 返回初始圖片位置
+        Object.keys(images).forEach((key, index) => {
+          gsap.to(imageRefs.current[index], {
+            x: images[key].initial.x,
+            y: images[key].initial.y,
+            duration: 1.5,
+            ease: "power3.out",
+          });
         });
 
         isCentered.current = false;
+        setScrollLocked(false);
       }
     };
 
     window.addEventListener("wheel", (event) => {
       if (scrollLocked) {
-        // 當滾動被鎖定時，將滾動範圍限制在 windowHeight
         window.scrollTo(0, windowHeight);
       } else {
         handleScroll(event);
@@ -333,39 +345,17 @@ const GSAPWrapper: React.FC = () => {
 
   return (
     <Wrapper>
-      <SVGContainer viewBox={`0 0 ${windowWidth} ${windowHeight}`}>
-        <OuterCircle
-          ref={outerCircleRef}
-          cx={centerX}
-          cy={centerY}
-          r={initialOuterCircleRadius}
-          fill="none"
-          stroke="gray"
-        />
-        <InnerCircle
-          ref={innerCircleRef}
-          cx={centerX}
-          cy={centerY}
-          r={initialInnerCircleRadius}
-          fill="none"
-          stroke="gray"
-        />
-        <Ellipse
-          ref={ellipseRef}
-          cx={centerX}
-          cy={centerY + 200}
-          rx="0"
-          ry="0"
-          fill="none"
-          stroke="#b0b0b0"
-        />
-      </SVGContainer>
       <WordsContainer>
         {nutritionWords.map((word, index) => (
-          <Word key={index} ref={(el) => (wordRefs.current[index] = el)}>
-            {word}
+          <Word
+            key={index}
+            ref={(el) => (wordRefs.current[index] = el)}
+            style={{ zIndex: word.zIndex }} // 將 zIndex 應用到樣式中
+          >
+            {word.letter} {/* 渲染 letter */}
           </Word>
         ))}
+
         <BalanceContainer>
           {balanceWords.map((word, index) => (
             <Word
@@ -373,54 +363,38 @@ const GSAPWrapper: React.FC = () => {
               ref={(el) =>
                 (wordRefs.current[index + nutritionWords.length] = el)
               }
+              style={{ zIndex: word.zIndex }} // 將 zIndex 應用到樣式中
             >
-              {word}
+              {word.letter} {/* 渲染 letter */}
             </Word>
           ))}
         </BalanceContainer>
       </WordsContainer>
-
-      <CenterImgContainer>
-        <CenterImg ref={centerImgRef} src={centerImg} />
-      </CenterImgContainer>
-      <PlanetImgContainer ref={planetContainerRef}>
-        <ManImg src={standingMan} />
-        <PlanetImg src={centerImg} />
-      </PlanetImgContainer>
-      <CubeImgContainer>
-        <InnerCubeImg ref={InnerCubeImgRef} src={cubeImg} />
-      </CubeImgContainer>
-      <CubeImgContainer>
-        <OuterCubeImg ref={OuterCubeImgRef} src={cubeImg} />
-      </CubeImgContainer>
-      <FoodImg src={avocado} />
+      <ImageContainer>
+        {Object.keys(images).map((key, index) => (
+          <Image
+            key={index}
+            ref={(el) => (imageRefs.current[index] = el)}
+            src={images[key].src}
+            alt={key}
+            width={images[key].width}
+            height={images[key].height}
+            style={{ zIndex: images[key].zIndex }}
+          />
+        ))}
+      </ImageContainer>
     </Wrapper>
   );
 };
 
-// 定義樣式
+// 樣式定義
 const Wrapper = styled.div`
-  height: 100vh;
+  height: 100vh; /* 增加頁面高度以觸發滾動 */
   width: 100%;
   position: relative;
   overflow: hidden;
 `;
 
-const OuterCircle = styled.circle`
-  transition: cx 1s, cy 1s, r 1s; /* 加入動畫效果 */
-`;
-
-const InnerCircle = styled.circle`
-  transition: cx 1s, cy 1s, r 1s; /* 加入動畫效果 */
-`;
-const SVGContainer = styled.svg`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  z-index: -3;
-`;
 const WordsContainer = styled.div`
   display: flex;
   position: absolute;
@@ -431,70 +405,32 @@ const WordsContainer = styled.div`
   align-items: center;
   margin: auto;
 `;
+
 const Word = styled.div`
   position: absolute;
   font-family: "Fira Code", monospace;
   font-size: 24px;
   font-weight: bold;
-  z-index: 0;
+  z-index: 1;
 `;
+
+const ImageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  z-index: 1;
+`;
+
+const Image = styled.img`
+  position: absolute;
+  max-width: 100%;
+  transform-origin: top;
+`;
+
 const BalanceContainer = styled.div`
   margin-left: 10rem;
 `;
 
-const CenterImgContainer = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  transform: translate(-50%, -50%);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: -1;
-`;
-
-const CenterImg = styled.img`
-  width: 150px;
-  height: auto;
-`;
-
-const PlanetImgContainer = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: -2;
-`;
-
-const PlanetImg = styled.img`
-  width: 500px;
-  height: auto;
-`;
-const CubeImgContainer = styled.div`
-  position: absolute;
-  z-index: 3;
-`;
-const InnerCubeImg = styled.img`
-  width: 50px;
-  height: auto;
-`;
-const OuterCubeImg = styled.img`
-  width: 100px;
-  height: auto;
-`;
-const Ellipse = styled.ellipse`
-  transition: rx 1s, ry 1s;
-`;
-const ManImg = styled.img`
-  position: absolute;
-  top: 50px;
-  right: 100px;
-  height: 140px;
-`;
-const FoodImg = styled.img`
-  display: flex;
-  position: absolute;
-  width: 200px;
-  margin: 0 auto;
-`;
 export default GSAPWrapper;
