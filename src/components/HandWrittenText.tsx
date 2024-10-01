d3242222dee79c438d2d7206fcb7c4068ffa17c2
Kevin Lin <minecrafts96628@gmail.com>
2024-10-01 18:02:20 +0800

    ensure handwritten text will change with window size

--- src/components/HandWrittenText.tsx
+++ src/components/HandWrittenText.tsx
@@ -9,6 +9,10 @@ interface HandwrittenTextProps {
   fillStyle?: string;
   color?: string;
   fontSize?: number;
+  width?: string;
+  height?: string;
+  minWidth?: string;
+  minHeight?: string;
 }
 
 const HandwrittenText = ({
@@ -17,11 +21,13 @@ const HandwrittenText = ({
   fill = "gray",
   fillStyle = "hachure",
   color = "black",
-  fontSize = 100, // 字體大小設置為100px
+  fontSize = 100,
+  width = "100%",
+  height = "100%",
 }: HandwrittenTextProps) => {
   const [paths, setPaths] = useState<string[]>([]);
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
+  const [viewBoxWidth, setViewBoxWidth] = useState(0);
+  const [viewBoxHeight, setViewBoxHeight] = useState(0);
 
   useEffect(() => {
     let ignore = false;
@@ -34,13 +40,13 @@ const HandwrittenText = ({
         if (ignore) return;
         const font = opentype.parse(buffer);
         const fontPaths = font
-          .getPaths(text, 0, 100, fontSize)
+          .getPaths(text, 0, fontSize, fontSize)
           .map((path: { toPathData: () => string }) => path.toPathData());
-        const textWidth = font.getAdvanceWidth(text, fontSize); // 使用原始 fontSize 計算寬度
-        const textHeight = fontSize * 1.2; // 將高度設為字體大小的 1.2 倍
+        const textWidth = font.getAdvanceWidth(text, fontSize);
+        const textHeight = fontSize * 1.2;
         setPaths(fontPaths);
-        setWidth(textWidth);
-        setHeight(textHeight);
+        setViewBoxWidth(textWidth + fontSize * 0.2);
+        setViewBoxHeight(textHeight);
       } catch (error) {
         console.error("Error loading font:", error);
       }
@@ -55,10 +61,11 @@ const HandwrittenText = ({
     <RoughSVG style={{ color }} options={{ roughness, fillStyle }}>
       <svg
         style={{
-          width: `${width}px`,
-          height: `${height}px`,
+          width: width,
+          height: height,
         }}
-        viewBox={`0 0 ${width * 1.2} ${Math.max(200, fontSize * 1.2)}`}
+        viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}
+        preserveAspectRatio="xMidYMid meet"
         fill={fill}
         stroke={color}
       >
