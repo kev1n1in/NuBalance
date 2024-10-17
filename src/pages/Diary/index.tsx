5fb091ef95aa084edb91a625d1725ec590acad3e
Kevin Lin <minecrafts96628@gmail.com>
2024-10-17 12:07:34 +0800

    refactor:extract diary meals selector.

--- src/pages/Diary/index.tsx
+++ src/pages/Diary/index.tsx
@@ -38,18 +38,13 @@ import polaroid from "./polaroid.png";
 import useAlert from "../../hooks/useAlertMessage";
 import debounce from "lodash.debounce";
 import RequiredMark from "../../components/RequiredMark";
+import MealSelector from "../../components/MealSelector";
+import { MealItem } from "../../types/mealTypes";
 
 type FoodItem = {
   id: string;
   food_name: string;
   food_info: string[];
-};
-
-type MealItem = {
-  id: string;
-  name: string;
-  imgSrc: string;
-  selectImgSrc: string;
 };
 
 type MoodItem = {
@@ -160,10 +155,9 @@ const Diary = () => {
   useEffect(() => {
     const inputElement = inputRef.current;
     if (inputElement) {
-      // 根據 scrollWidth 動態設置 input 的寬度
       inputElement.style.width = `${inputElement.scrollWidth}px`;
     }
-  }, [inputValue]); // 當 inputValue 改變時重新計算寬度
+  }, [inputValue]);
   const handleMouseEnter = (index: number) => {
     if (titleRefs.current[index] && !annotations[index]) {
       const newAnnotation = annotate(titleRefs.current[index]!, {
@@ -324,7 +318,6 @@ const Diary = () => {
           </UploadBox>
         </MobileImageUploadContainer>
         <MealSelectorWrapper>
-          {" "}
           <MealSelectorTitle
             ref={(el) => (titleRefs.current[0] = el)}
             onMouseEnter={() => handleMouseEnter(0)}
@@ -332,27 +325,11 @@ const Diary = () => {
           >
             Meal Selector <RequiredMark />
           </MealSelectorTitle>
-          <MealSelectorContainer>
-            {meals.map((meal) => (
-              <MealContainer
-                key={meal.id}
-                onClick={() => handleMealClick(meal)}
-              >
-                <Meal
-                  isSelected={selectedMeal?.id === meal.id}
-                  src={
-                    selectedMeal?.id === meal.id
-                      ? meal.selectImgSrc
-                      : meal.imgSrc
-                  }
-                  alt={meal.name}
-                />
-                <MealName isSelected={selectedMeal?.id === meal.id}>
-                  {meal.name}
-                </MealName>
-              </MealContainer>
-            ))}
-          </MealSelectorContainer>
+          <MealSelector
+            meals={meals}
+            selectedMeal={selectedMeal}
+            handleMealClick={handleMealClick}
+          ></MealSelector>
         </MealSelectorWrapper>
 
         <FoodAndTimePickerWrapper>
@@ -523,54 +500,6 @@ const MealSelectorWrapper = styled.div`
 `;
 const MealSelectorTitle = styled.h2`
   width: 230px;
-`;
-const MealSelectorContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  height: 200px;
-  align-items: center;
-  @media (max-width: 768px) {
-    display: grid;
-    grid-template-columns: 2fr 2fr;
-  }
-`;
-
-const MealContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0 10px;
-  cursor: pointer;
-  @media (max-width: 480px) {
-    margin: 0;
-  }
-  @media (max-width: 360px) {
-    margin-top: 12px;
-  }
-`;
-
-const Meal = styled(motion.img).attrs<{ isSelected: boolean }>(
-  ({ isSelected }) => ({
-    initial: { scale: 1 },
-    animate: { scale: isSelected ? 1.3 : 1 },
-    transition: { type: "spring", stiffness: 300 },
-  })
-)<{ isSelected: boolean }>`
-  width: 140px;
-  height: auto;
-  @media (max-width: 360px) {
-    width: 80px;
-  }
-`;
-
-const MealName = styled.span<{ isSelected: boolean }>`
-  margin-top: 8px;
-  font-size: 24px;
-  text-align: center;
-  color: ${({ isSelected }) => (isSelected ? "#a23419" : "black")};
-  @media (max-width: 360px) {
-    font-size: 20px;
-  }
 `;
 
 const FoodAndTimePickerWrapper = styled.div`
